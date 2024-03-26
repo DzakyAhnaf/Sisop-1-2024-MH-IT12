61e860c38dc377f4bc4174be6a1cefb12ebae2f7
adlyatarisa <164857172+adlyatarisa@users.noreply.github.com>
2024-03-26 14:10:42 +0700

    minute_log.sh

--- soal_4/minute_log.sh
+++ soal_4/minute_log.sh
@@ -9,6 +9,6 @@ mem=$(free -m | grep Mem | awk '{print $2,",",$3,",",$4,",",$5,",",$6,",",$7}')
 swap=$(free -m | grep Swap | awk '{print $2,",",$3,",",$4}')
 path=$(du -sh ~ | awk '{print $2,",",$1}')
 
-echo $mem,$swap,$dat3 >> log/metrics_$timestamp.log
+echo $mem,$swap,$path >> log/metrics_$timestamp.log
 
 chmod 600 /home/tarisa/log/metrics_$timestamp.log
